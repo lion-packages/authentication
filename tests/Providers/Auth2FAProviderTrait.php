e12a8dfe201d44926fe35c81c798953a417c9266
Sleon4 <sergioleon4004@hotmail.com>
2025-03-27 10:31:10 -0500

    ci: the infection is added to Github Actions

--- tests/Providers/Auth2FAProviderTrait.php
+++ tests/Providers/Auth2FAProviderTrait.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 namespace Tests\Providers;
 
-use Tests\Constants;
-
 trait Auth2FAProviderTrait
 {
     /**
--- tests/Providers/Constants.php
+++ tests/Providers/Constants.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace Tests;
+namespace Tests\Providers;
 
 class Constants
 {
